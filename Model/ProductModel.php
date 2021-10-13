e26146e63fe33bd382acf310c61fe51cf0820b42
ChusCharet <91920133+ChusCharet@users.noreply.github.com>
2021-10-13 16:07:13 -0300

    Update ProductModel.php
    se agrega showhome, y las funciones insert product, delete y update que fueron sacadas del archivo db.php
    update a revisar

--- Model/ProductModel.php
+++ Model/ProductModel.php
@@ -7,13 +7,31 @@ class ProductModel{
         $this->db = new PDO('mysql:host=localhost;'.'dbname=db_tpe;charset=utf8', 'root', ''); 
         
     }
-
+    function showHome(){
+    //a realizar aun    
+    }
+    
     function getProducts(){
         $sentencia = $this->db->prepare("select * from producto");
         $sentencia->execute();
         $producto = $sentencia->fetchAll(PDO::FETCH_OBJ);
         return $producto;
     
+    }
+    // esto lo saque de db.php
+    function insertProduct($precio, $color, $tamanio, $fk_id_estilo) {
+        $sentencia = $this->db->prepare("INSERT INTO producto(precio, color, tamanio, fk_id_estilo) VALUES(?, ?, ?, ?)");
+        $sentencia->execute(array($precio, $color, $tamanio, $fk_id_estilo));
+    
+    }
+    function deleteProductFromDb ($id) {
+        $sentencia = $this->db->prepare("DELETE FROM producto WHERE id_producto=?");
+        $sentencia->execute(array($id));
+    }
+    
+    function updateProductFromDB($id){
+        $sentencia = $this->db->prepare("UPDATE  `id_producto` SET `id_produto`='[value-1]',`Color`='[value-2]',`Precio`='[value-3]',`Tamaño`='[value-4]',`fk_id_categoria`='[value-5]' WHERE 1");
+        $sentencia->execute(array($id));
     }
 
 }
