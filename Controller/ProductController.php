135ed32cd32f1b8e4396ec38b7407e05117ea525
ChusCharet <91920133+ChusCharet@users.noreply.github.com>
2021-10-13 16:03:41 -0300

    Update ProductController.php
    se agregaron las funcionees delete, update(esta comentada), view y creat products
    se corrigio la showhome a showproducts y se inicia la funcion show home para que maneje la vista ppal sin los productos

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -2,25 +2,52 @@
 
 require_once "Model/ProductModel.php";
 require_once "View/ProductView.php";
+require_once "./AuthHelper.php";
 
 class ProductController {
 
     private $model;
     private $view;
+    private $authHelper;
 
     function __construct() {
         $this->model = new ProductModel();
         $this->view = new ProductView();
+        $this->AuthHelper = new AuthHelper();
         
     }
-
-
     function showHome() {
-
         $products = $this->model->getProducts();
         $this->view->showProducts($products);
-
-        
-    
+         $home = $this->model->showHome();
     }
+    
+    function showProducts() { //antigua showhome
+        $this->authHelper->checkLoggedIn();
+        $products = $this->model->getProducts();
+        $this->view->showProducts($products);    
+    }
+    function createProduct($id){
+        $this->authHelper->checkLoggedIn();
+        $this->model->insertProduct($precio, $color, $tamanio, $fk_id_estilo);
+        $this->view->showHomeLocation();
+    }
+    function deleteProduct($id){
+        $this->authHelper->checkLoggedIn();
+        $this->model->deleteProductFromDB($id);
+        $this->view->showHomeLocation();
+    }
+    // function updateProduct($id){
+    //     $this->authHelper->checkLoggedIn();
+    //     $this->model->updateProductFromDB($id);
+    //     $this->view->showHomeLocation();
+    // }
+     function viewProduct($id){
+        $this->authHelper->checkLoggedIn();
+        $product = $this->model->getProducts();
+        $this->view->showProduct($product);
+    }
+
+    
+    
 }
